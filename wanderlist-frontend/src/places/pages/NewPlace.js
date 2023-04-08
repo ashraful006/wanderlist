18478fd098e94a6ac3ab68e9afd24c253a96f6bd
ashraful006 <ashraful.p314159@gmail.com>
2023-04-09 02:26:20 +0600

    feature/input-state: Manage input states

--- wanderlist-frontend/src/places/pages/NewPlace.js
+++ wanderlist-frontend/src/places/pages/NewPlace.js
@@ -6,7 +6,7 @@ import { Input } from "../../shared/components/FormElements/Input";
 const NewPlace = () => {
   return (
     <form className="place-form">
-      <Input element="input" label="Title" />
+      <Input element="input" label="Title" validators={[]} errorText="Please enter a valid text" />
     </form>
   );
 };
--- wanderlist-frontend/src/shared/components/FormElements/Input.js
+++ wanderlist-frontend/src/shared/components/FormElements/Input.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useReducer } from "react";
 
 import "./Input.css";
 
 export const Input = (props) => {
+
+  const inputReducer = (state, action) => {
+    switch (action.type) {
+      case "CHANGE":
+        return {
+          ...state,
+          value: action.val,
+          isValid: true,
+        };
+
+      default:
+        return state;
+    }
+  }
+  const [inputState, dispatch] = useReducer(inputReducer, { value: "", isValid: false });
+
+  const changeHandler = event => {
+    dispatch({ type: "CHANGE", val: event.target.value })
+  }
+
   const element =
     props.element === "input" ? (
-      <input id={props.id} placeholder={props.placeholder} name={props.name} />
+      <input id={props.id} placeholder={props.placeholder} name={props.name} onChange={changeHandler} value={inputState.value} />
     ) : (
-      <textarea id={props.id} rows={props.rows || 3} />
+      <textarea id={props.id} rows={props.rows || 3} onChange={changeHandler} value={inputState.value} />
     );
   return (
-    <div className="form-control">
+    <div className={`form-control ${!inputState.isValid && 'form-control--invalid'}`}>
       <label htmlFor={props.id}>{props.label}</label>
       {element}
+      {!inputState.isValid && <p>{props.errorText}</p>}
     </div>
   );
 };
