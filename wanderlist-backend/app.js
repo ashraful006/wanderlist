692b99816b6b07dd132649db4f66f3b96cfcb77c
ashraful006 <ashraful.p314159@gmail.com>
2023-05-02 00:15:18 +0600

    Add unknown url error handler

--- wanderlist-backend/app.js
+++ wanderlist-backend/app.js
@@ -2,12 +2,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const placeRoutes = require("./routes/places-routes");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use("/api/places", placeRoutes);
+
+app.use((req, res, next) => {
+  return next(new HttpError("Could not find given route", 404));
+});
 
 app.use((error, req, res, next) => {
   if (res.headersSent) {
