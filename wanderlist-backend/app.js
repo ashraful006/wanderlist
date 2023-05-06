01c86b84eabb5574a69c2be00a12baecdb415f8a
ashraful006 <ashraful.p314159@gmail.com>
2023-05-06 20:22:03 +0600

    Create users api

--- wanderlist-backend/app.js
+++ wanderlist-backend/app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const placeRoutes = require("./routes/places-routes");
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
 const HttpError = require("./models/http-error");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use("/api/places", placeRoutes);
+app.use("/api/places", placesRoutes);
+app.use("/api/users", usersRoutes);
 
 app.use((req, res, next) => {
   return next(new HttpError("Could not find given route", 404));
---
+++ wanderlist-backend/controllers/users-controllers.js
@@ -0,0 +1,57 @@
+const uuid = require("uuid");
+
+const HttpError = require("../models/http-error");
+
+const DUMMY_USERS = [
+  {
+    id: "u1",
+    name: "Md. Ashraful Islam",
+    email: "asif@gmail.com",
+    password: "testpassword",
+  },
+];
+
+const getUsers = (req, res, next) => {
+  res.json({ users: DUMMY_USERS });
+};
+
+const signup = (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  const hasUser = DUMMY_USERS.find((user) => user.email === email);
+
+  if (hasUser) {
+    return next(
+      new HttpError(
+        "This email is already registered, please use another email.",
+        422
+      )
+    );
+  }
+  const newUser = {
+    id: uuid.v4(),
+    name,
+    email,
+    password,
+  };
+
+  DUMMY_USERS.push(newUser);
+
+  res.status(201).json({ user: newUser });
+};
+
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  const identifiedUser = DUMMY_USERS.find((user) => user.email === email);
+
+  if (!identifiedUser || identifiedUser.password !== password) {
+    return next(new HttpError("Given email and password seems to be wrong!"));
+  }
+
+  res.json({ message: "Logged in" });
+};
+
+exports.getUsers = getUsers;
+exports.signup = signup;
+exports.login = login;
