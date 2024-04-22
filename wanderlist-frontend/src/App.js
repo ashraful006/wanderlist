41849e719eac9e47ec1d7bd9b638d4d51d4e2da8
ashraful006 <ashraful.p314159@gmail.com>
2024-04-22 12:32:56 +0600

    Handle login expiration time

--- wanderlist-frontend/src/App.js
+++ wanderlist-frontend/src/App.js
@@ -18,19 +18,21 @@ function App() {
   const [token, setToken] = useState(false);
   const [userId, setUserId] = useState(null);
 
-  const logIn = useCallback((userId, token) => {
+  const logIn = useCallback((userId, token, expirationTime) => {
     setToken(token);
-    localStorage.setItem('userData', JSON.stringify({userId: userId, token: token}));
+    const tokenExpirationTime = expirationTime || new Date(new Date().getTime() + 1000 * 60 * 60);
+    localStorage.setItem('userData', JSON.stringify({ userId: userId, token: token, expirationTime: tokenExpirationTime }));
     setUserId(userId);
   }, []);
 
   useEffect(() => {
     const storedUserData = JSON.parse(localStorage.getItem('userData'));
 
-    if (storedUserData && storedUserData.token) {
+    if (storedUserData && storedUserData.token && new Date(storedUserData.expirationTime) > new Date()) {
       logIn(storedUserData.userId, storedUserData.token);
     }
   }, [logIn]);
+
   const logOut = useCallback(() => {
     setToken(null);
     setUserId(null);
